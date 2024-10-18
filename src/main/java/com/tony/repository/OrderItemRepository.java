f066650a7258214810909c0d3784d6de15e42a86
Tonyyyy <ea103java28@gmail.com>
2024-10-18 22:59:31 +0800

    Delete some that not use

--- src/main/java/com/tony/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tony.repository;
-
-import com.tony.model.OrderItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderItemRepository extends CrudRepository<OrderItem, Integer> {
-}
--- src/main/java/com/tony/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tony.repository;
-
-import com.tony.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-}
